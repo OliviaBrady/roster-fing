9f721c9cb85ccc92b99315048cd68fa16d2f8d93
Olivia Brady <olivia@Olivias-MacBook-Air.local>
2021-07-25 15:16:47 +1000

    unfucked times

--- Learning.py
+++ Learning.py
@@ -1,5 +1,5 @@
 from ics import Calendar, Event
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil import tz
 
 c = Calendar()
@@ -15,21 +15,39 @@ def to_utc(date_string):
     time_dt = datetime.strptime(date_string, date_format).replace(tzinfo=tz.gettz('Australia/Sydney'))
     time_dt = time_dt.astimezone(tz.tzutc())
 
+    return time_dt
 
 
 with open('Example Roster.html', 'r') as my_file:
     lines = my_file.readlines()
 
+    dates = []
+    start_times = []
+
     for line in lines:
+
         if 'li data-role="list-divider"' in line:
             day, mth, year = line.split('>')[1].split('-')[0].split(' ')[1].split('/')
 
-            e = Event()
-            e.name = "Shift"
-            time = f"{year}-{mth}-{day} 07:00:00"
-            e.begin = to_utc(time)
-            e.duration = {'hours':12}
-            c.events.add(e)
+            date = (day, mth, year)
+            dates.append(date)
+
+        elif '19:00 - 07:30' in line:
+            start_times.append('19:00:00')
+
+        elif '07:00 - 19:30' in line:
+            start_times.append('07:00:00')
+
+    for date, start_time in zip(dates, start_times):
+
+        e = Event()
+        e.name = "Shift"
+        time = f"{year}-{mth}-{day} {start_time}"
+        e.begin = to_utc(time)
+        e.duration = {"hours" : 12, "minutes" : 30}
+        c.events.add(e)
 
 with open('night-shift.ics', 'w') as my_file:
     my_file.writelines(c)
+
+print('completed successfully')
